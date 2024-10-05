d4ddde826fab0a7354c75c0724a10a1e2e8d1f38
trisnodonny <trisnodonny@gmail.com>
2024-10-05 11:46:37 +0700

    update

---
+++ src/components/partial/Journey.jsx
@@ -0,0 +1,5 @@
+export default function Journey() {
+  return (
+    <section className="journey"></section>
+  )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import Hero from "@components/partial/Hero";
 import Introduction from "@components/partial/Introduction";
+import Journey from "@components/partial/Journey";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
       <Introduction />
+      <Journey />
     </main>
   );
 }
